45f9eaead1d0fc4b78be8c16347016eaffef0e9f
Javier Cuevas <alexja98@hotmail.com>
2020-11-04 16:01:15 -0600

    Migración para creación de tabla libros

---
+++ database/migrations/2020_11_04_211548_create_libros_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLibrosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('libros', function (Blueprint $table) {
+            $table->string('isbn', 13);
+            $table->string('nombre', 100);
+            $table->string('autor', 255)->default('anonimo');
+            $table->string('editorial', 100);
+            $table->unsignedTinyInteger('edicion');
+            $table->year('anio');
+            $table->unsignedSmallInteger('paginas');
+            $table->timestamps();
+
+
+            $table->primary('isbn');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('libros');
+    }
+}
